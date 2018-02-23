563f5e4f3db2a3b786b0dad7929cbdca2bee1fef
James Little <littleguy23@gmail.com>
2018-02-23 12:04:35 +0100

    Finish triangle orbiting

--- js/Scene.js
+++ js/Scene.js
@@ -7,8 +7,11 @@ const Scene = function(gl) {
 
   this.timeAtLastFrame = new Date().getTime();
 
-  this.trianglePosition = new Vec3(0, 1, 0);
+  this.trianglePosition = new Vec3(0.2, 0.3, 0);
   this.rotation = 0;
+  this.scale = -0.5;
+
+  this.orbiters = 5
 };
 
 Scene.prototype.update = function(gl, keysPressed) {
@@ -17,19 +20,19 @@ Scene.prototype.update = function(gl, keysPressed) {
   this.timeAtLastFrame = timeAtThisFrame;
 
   if(this.keysPressed.W) {
-    this.trianglePosition.add(0, 3*dt, 0);
+    this.trianglePosition.add(0, 1*dt, 0);
   }
 
   if(this.keysPressed.A) {
-    this.trianglePosition.add(-3*dt, 0, 0);
+    this.trianglePosition.add(-1*dt, 0, 0);
   }
 
   if(this.keysPressed.S) {
-    this.trianglePosition.add(0, -3*dt, 0);
+    this.trianglePosition.add(0, -1*dt, 0);
   }
 
   if(this.keysPressed.D) {
-    this.trianglePosition.add(3*dt, 0, 0);
+    this.trianglePosition.add(1*dt, 0, 0);
   }
 
   if(this.keysPressed.Q) {
@@ -40,28 +43,50 @@ Scene.prototype.update = function(gl, keysPressed) {
     this.rotation += -1*dt;
   }
 
+  if(this.keysPressed.Z) {
+    this.scale += -1*dt;
+  }
+
+  if(this.keysPressed.C) {
+    this.scale += 1*dt;
+  }
+
+  if(this.keysPressed.R) {
+    this.orbiters += 1;
+  }
+
+  if(this.keysPressed.F) {
+    this.orbiters -= 1;
+  }
+
   const trianglePositionLocation = gl.getUniformLocation(this.solidProgram.glProgram, "trianglePosition");
   const matrixLocation = gl.getUniformLocation(this.solidProgram.glProgram, "modelMatrix");
 
-  if(trianglePositionLocation < 0 || matrixLocation < 0) {
-    console.log("Could not find uniform");
-  } else {
-    const modelMatrix = new Mat4().rotate(this.rotation).translate(this.trianglePosition);
-    this.trianglePosition.commit(gl, trianglePositionLocation);
-    modelMatrix.commit(gl, matrixLocation);
-  }
-
-  // clear the screen
+  // Clear screen
   gl.clearColor(0.3, 0.0, 0.3, 1.0);
   gl.clearDepth(1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  this.solidProgram.commit();
-  this.triangleGeometry.draw();
+  if(trianglePositionLocation < 0 || matrixLocation < 0) {
+    console.log("Could not find uniform");
+  } else {
+    const modelMatrix = new Mat4().rotate(this.rotation).scale(Math.pow(3, this.scale)).translate(this.trianglePosition);
+    this.trianglePosition.commit(gl, trianglePositionLocation);
+    modelMatrix.commit(gl, matrixLocation);
+    this.solidProgram.commit();
+    this.triangleGeometry.draw();
 
-  // this.trianglePosition2.commit(gl, trianglePositionLocation)
-  // gl.uniform1f(triangleScaleLocation, this.triangleScale2);
-  // this.triangleGeometry.draw();
+    for(var i=0; i<this.orbiters; i++) {
+      const modelMatrix2 = new Mat4()
+        .rotate(timeAtThisFrame/700)
+        .scale(0.25)
+        .translate(0, 0.5, 0)
+        .rotate(2*Math.PI / this.orbiters * i + (timeAtThisFrame / 700))
+        .translate(this.trianglePosition);
+      modelMatrix2.commit(gl, matrixLocation);
+      this.triangleGeometry.draw();
+    }
+  }
 };
 
 
